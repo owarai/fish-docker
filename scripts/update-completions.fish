28c145645721abd83536c40e813c44b7de4ac370
max <dingfengwang@yunify.com>
2021-03-26 16:11:06 +0800

    Make plugin works as expected
    1. Adjust completion init method, create when install;
    2. Remove completions/abbrs when uninstall.

--- scripts/update-completions.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-#
-# Copyright (c) 2020 Rich Lewis
-# License: MIT
-
-curl https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish > (dirname (status -f))/../completions/docker.fish
-
-curl https://raw.githubusercontent.com/docker/compose/master/contrib/completion/fish/docker-compose.fish > (dirname (status -f))/../completions/docker-compose.fish
-
